0fc16a8347c4c9d0e86d2f0529aaa21b4f5ac512
idoenk <1d03nk@gmail.com>
2016-07-24 21:10:16 +0700

    adjust targetsearch; debug-mode;

--- sgoogle.user.js
+++ sgoogle.user.js
@@ -9,7 +9,7 @@
 // @include         http://www.google.*
 // @include         https://www.google.*
 // @include         https://encrypted.google.*
-// @version         0.2.2
+// @version         0.3
 // @timestamp       1444128908196
 // @run-at          document-start
 // ==/UserScript==
@@ -21,39 +21,16 @@
 window.alert(new Date().getTime());
 *
 */
+!function init_sgoogle(){
+var gvar = function(){};
+
+gvar.__DEBUG__ = !1;
 
 // Do not run on iframes > image search
 if (window.top !== window.self) {
   return;
 }
 
-function clog(x){console.log(x)}
-var $D=function (q, root, single) {
-    var el;
-    if (root && typeof root == 'string') {
-        root = $D(root, null, true);
-        if (!root) { return null; }
-    }
-    if( !q ) return false;
-    if ( typeof q == 'object') return q;
-    root = root || document;
-    if (q[0]=='/' || (q[0]=='.' && q[1]=='/')) {
-        if (single) {
-          return document.evaluate(q, root, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-        }
-        return document.evaluate(q, root, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-    }
-    else if (q[0]=='.') {
-      el = root.getElementsByClassName(q.substr(1));
-      return single ? el[0] : el;
-    }
-    else if( q[0]=='#' ){
-      return root.getElementById( q.substr(1) );
-    }
-    else{
-        return root.querySelector(q);
-    }
-};
 // native add - remove element
 var Dom = {
   remove: function(el) {
@@ -80,7 +57,34 @@ var Dom = {
     }
   }(),
 };
-var GM_addGlobalStyle = function (a, b, c) {
+function clog(x){if(gvar.__DEBUG__) console.log(x)}
+function $D(q, root, single) {
+    var el;
+    if (root && typeof root == 'string') {
+        root = $D(root, null, true);
+        if (!root) { return null; }
+    }
+    if( !q ) return false;
+    if ( typeof q == 'object') return q;
+    root = root || document;
+    if (q[0]=='/' || (q[0]=='.' && q[1]=='/')) {
+        if (single) {
+          return document.evaluate(q, root, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        }
+        return document.evaluate(q, root, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    }
+    else if (q[0]=='.') {
+      el = root.getElementsByClassName(q.substr(1));
+      return single ? el[0] : el;
+    }
+    else if( q[0]=='#' ){
+      return root.getElementById( q.substr(1) );
+    }
+    else{
+        return root.querySelector(q);
+    }
+};
+function GM_addGlobalStyle(a, b, c) {
   var d, e;
   if (a.match(/^https?:\/\/.+/)) {
     d = createEl("link", { type: "text/css", rel:'stylesheet', href:a });
@@ -162,36 +166,38 @@ function setAttr(name, value, Obj){
     return Obj.setAttribute(name, value);
 }
 
+// main
 (function(window, document){
-  var i = '!important';
-  var top2nd = '33px';
-  var hlh2nd = '30px';
-  var grey_trans = 'background-color: rgba(241, 241, 241, 0.8)';
-  var white_trans = 'background-color:rgba(255,255,255, 0.7878)';
-  var bkwhite = 'background-color:#fff';
-  var css = ''
-    +'body.stgoogfixed #searchform{position:fixed'+i+'; top:6px'+i+'; cursor: n-resize;}'
-    +'body.stgoogfixed #cnt{padding-top:70px'+i+';}'
-    +'body.stgoogfixed div#gb + div, body.stgoogfixed #gb + div > div{height: 48px;'+grey_trans+';}'
-    +'body.stgoogfixed #sfdiv, body.stgoogfixed #sfdiv > div:first-child{background-color: transparent;}'
-    +'body.stgoogfixed #sfdiv.sbfcn, body.stgoogfixed #searchform:hover #sfdiv{'+bkwhite+';}'
-    +'body.stgoogfixed #searchform:hover div#gb + div{background:#f1f1f1;}'
-    +'body.stgoogfixed div#top_nav{position: fixed;top: 40px;width: 100%;height: '+top2nd+'; z-index:101;}'
-    +'body.stgoogfixed div#top_nav, body.stgoogfixed div#hdtbSum{'+white_trans+';}'
-    +'body.stgoogfixed div#top_nav:hover, body.stgoogfixed #searchform:hover + #main div#top_nav{'+bkwhite+';}'
-    +'body.stgoogfixed div#hdtbSum{height: '+top2nd+';line-height: '+hlh2nd+';position:absolute; width:100%;}'
-    +'body.stgoogfixed div#hdtb-msb .hdtb-mitem.hdtb-msel, body.stgoogfixed #hdtb-msb .hdtb-mitem.hdtb-msel-pre{height: '+hlh2nd+';}'
-    +'body.stgoogfixed .hdtb-mn-o, body.stgoogfixed #hdtbMenus.hdtb-td-o{top:'+top2nd+i+';}'
-    +''
+  var id_css = 'stgoogfixed',
+      i = '!important',
+      top2nd = '33px',
+      hlh2nd = '30px',
+      grey_trans = 'background-color: rgba(241, 241, 241, 0.8)',
+      white_trans = 'background-color:rgba(255,255,255, 0.7878)',
+      bkwhite = 'background-color:#fff',
+      basic_css = '',
+      css = ''
+        +'body.stgoogfixed #searchform{position:fixed'+i+'; top:6px'+i+'; cursor: n-resize;}'
+        +'body.stgoogfixed #cnt{padding-top:70px'+i+';}'
+        +'body.stgoogfixed div#gb + div, body.stgoogfixed #gb + div > div{height: 48px;'+grey_trans+';}'
+        +'body.stgoogfixed #sfdiv, body.stgoogfixed #sfdiv > div:first-child{background-color: transparent;}'
+        +'body.stgoogfixed #sfdiv.sbfcn, body.stgoogfixed #searchform:hover #sfdiv{'+bkwhite+';}'
+        +'body.stgoogfixed #searchform:hover div#gb + div{background:#f1f1f1;}'
+        +'body.stgoogfixed div#top_nav{position: fixed;top: 40px;width: 100%;height: '+top2nd+'; z-index:101;}'
+        +'body.stgoogfixed div#top_nav, body.stgoogfixed div#hdtbSum{'+white_trans+';}'
+        +'body.stgoogfixed div#top_nav:hover, body.stgoogfixed #searchform:hover + #main div#top_nav{'+bkwhite+';}'
+        +'body.stgoogfixed div#hdtbSum{height: '+top2nd+';line-height: '+hlh2nd+';position:absolute; width:100%;}'
+        +'body.stgoogfixed div#hdtb-msb .hdtb-mitem.hdtb-msel, body.stgoogfixed #hdtb-msb .hdtb-mitem.hdtb-msel-pre{height: '+hlh2nd+';}'
+        +'body.stgoogfixed .hdtb-mn-o, body.stgoogfixed #hdtbMenus.hdtb-td-o{top:'+top2nd+i+';}',
+      el = null
   ;
   
-  var basic_css = ''
-  +'#searchform, #hdtbSum, #top_nav{ transition: top 220ms ease-in-out; -webkit-transition: top 220ms ease-in-out;}'
-  +'#topalert [href*="alerts"]{margin-bottom:0;}'
+  basic_css = ''
+    +'#searchform, #hdtbSum, #top_nav{ transition: top 220ms ease-in-out; -webkit-transition: top 220ms ease-in-out;}'
+    +'#topalert [href*="alerts"]{margin-bottom:0;}'
   ;
   GM_addGlobalStyle(basic_css);
   
-  var el, id_css = 'stgoogfixed';
   
   if( !$D('#'+id_css) )
     GM_addGlobalStyle(css, id_css);
@@ -227,39 +233,29 @@ function setAttr(name, value, Obj){
       newEl = createEl("a", attr, el.innerHTML);
       par.replaceChild(newEl, el);
     }
-    //clog("els-len="+elen+'; cleanup done');
   }
 
   var observer = new MutationObserver(function(mutations) {
     var BreakException = {};
     try{
       mutations.forEach(function(mev) {
-        // var $el = $(mev.target);
         var lastNode, addedNodes = mev.addedNodes;
         if( addedNodes.length > 0 ){
           addedNodes = mev.target.lastElementChild;
-          //clog(addedNodes);
           lastNode = addedNodes.lastElementChild;
-          if( lastNode && (lastNode.id == 'ires') ){
-            // do cleanup...
-            setTimeout(function(){
-
-              cleanup_tracker($D('#search'));
-            }, 0);
+          if( lastNode && /^idb/i.test(lastNode.id) ){
+            
+            clog('lastNode');
+            clog(lastNode);
           }
         }
-        //clog(mev);
-        
       });
     }catch(e){}
   });
   
   document.addEventListener('DOMContentLoaded', function(){
 
-    var el, targetSearch;
-
-    targetSearch = $D('#search');
-    
+    var targetSearch = $D('#main'), el;
 
     // pass in the target node, as well as the observer options
     if( targetSearch )
@@ -276,14 +272,18 @@ function setAttr(name, value, Obj){
 
     
     // in news page?
-    el = $D('//a[contains(@href,"alerts")]', null, true);
+    el = $D('//a[contains(@href,"alerts") and contains(@class,"ab_button")]', null, true);
     if( el ){
-      var ol = $D("ol", $D("#hdtbSum"), true);
-      var clNode = el.cloneNode(true);
-      var innerLi = createEl('li', {"class":"ab_ctl","id":"topalert"});
-      innerLi.appendChild(clNode);
-      
-      ol.insertBefore(innerLi, ol.firstChild);
+      var ol = $D("ol", $D("#hdtbSum"), true),
+          clNode, innerLi
+      ;
+      if( ol ){
+        clNode = el.cloneNode(true);
+        innerLi = createEl('li', {"class":"ab_ctl","id":"topalert"});
+        innerLi.appendChild(clNode);
+        
+        ol.insertBefore(innerLi, ol.firstChild);
+      }
     }
     
     // events
@@ -305,5 +305,5 @@ function setAttr(name, value, Obj){
       }
     });
   }, false);
-    
 })(window, document);
+}();
